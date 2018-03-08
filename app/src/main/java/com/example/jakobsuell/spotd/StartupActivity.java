544584c3d45604208d1f045b5a44766733b91d2d
Anthony Dwyer <abd14@students.uwf.edu>
2018-03-08 00:55:09 -0600

    Fixes renamed activity causing broken manifest

--- app/src/main/java/com/example/jakobsuell/spotd/StartupActivity.java
+++ app/src/main/java/com/example/jakobsuell/spotd/StartupActivity.java
@@ -1,21 +1,18 @@
 package com.example.jakobsuell.spotd;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class StartMenu extends AppCompatActivity {
+public class StartupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
