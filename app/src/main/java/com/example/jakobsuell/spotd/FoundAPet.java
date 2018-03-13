16b3426a290df522d654eb4b59175e54a3842fa5
SAA37 <saa37@students.uwf.edu>
2018-03-13 15:15:16 -0500

    Added "Add a Pet" to MyPets_Activity Created navigation from MyPets_Activity to AddAPet_Activity when clicked

--- app/src/main/java/com/example/jakobsuell/spotd/FoundAPet.java
+++ app/src/main/java/com/example/jakobsuell/spotd/FoundAPet.java
@@ -17,15 +17,6 @@ public class FoundAPet extends AppCompatActivity {
         setContentView(R.layout.activity_found_apet);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     //Encapsulates ability to create itself
