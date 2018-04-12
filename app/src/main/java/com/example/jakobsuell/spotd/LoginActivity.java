dc17fa4ae8ad504dff69e32e41ba909ffa116a66
Anthony Dwyer <abd14@students.uwf.edu>
2018-04-12 00:04:51 -0500

    Flips the debug switch off

--- app/src/main/java/com/example/jakobsuell/spotd/LoginActivity.java
+++ app/src/main/java/com/example/jakobsuell/spotd/LoginActivity.java
@@ -22,7 +22,7 @@ import controllers.LoginController;
 public class LoginActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
-    private final boolean GO_DEBUG_ACTIVITY = true;     // shows debug activity instead of toolbar_menu_selector
+    private final boolean GO_DEBUG_ACTIVITY = false;     // shows debug activity instead of toolbar_menu_selector
     private FirebaseAuth auth;
     private String TAG = "LoginActivity";
     private TextView tvMessage;
